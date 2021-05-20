beb26036220288130653d11658469a34e03100bc
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-05-20 14:07:02 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,65 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact App</title>
-    <link rel = "stylesheet" href = "style.css">
-    <style>
-.table th , td{
-    border:1px solid black;
-    padding:10px;
-}
-    </style>
+    <link href="style.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+
 </head>
-<body class = "body">
-    <header class = "heading">
-        <h1>Contact App </h1>
-    </header>    
+
+<body class="body">
+    <header class="heading">
+        <h1>SAVE YOUR CONTACTS HERE</h1>
+    </header>
     <form action="adddata.php" method="post">
         <div class="main">
-            <label for="name">Name</label><br/>
-            <input  type="text" name="name" id="name" required><br/>
-            <label for="name">Number</label><br/>
-            <input type="number" name="phone" id="phone" required><br/><br/>
-            <input type = "submit" value = "save">
+            <div class="row">
+                <div class="name">
+                    <label class="label" for="name">Name:</label><br />
+                </div>
+                <div class="number">
+                    <input class="input" type="text" name="name" id="name" placeholder="name" required><br />
+                </div>
+            </div>
+            <div class="row">
+                <div class="name">
+                    <label class="label" for="number">Mobile Number:</label><br />
+                </div>
+                <div class="number">
+                    <input class="input" type="number" name="phone" id="phone" placeholder="number" required><br />
+                </div>
+            </div>
+            <div class="button">
+                <button class="save">SAVE</button>
+            </div>
 
         </div>
     </form>
     <hr>
-    <table class = "table">
-    <tr>
-    <th>Name</th>
-    <th>Phone No.</th>
-    <th>Actions</th>
-    </tr>
-    <?php
-        include 'db.php';
-        $sql = "SELECT * from names";
+    <div class="data">
+        <table class="table">
+            <tr>
+                <th>Name</th>
+                <th>Phone No.</th>
+                <th>Actions</th>
+            </tr>
+            <?php
+            include 'db.php';
+            $sql = "SELECT * from names";
 
-        $result = mysqli_query($con,$sql);
+            $result = mysqli_query($con, $sql);
 
-        if($result){
-            while($row = mysqli_fetch_assoc($result)){
-                $id = $row['id'];
-                $name = $row['name'];
-                $phone = $row['phone'];
+            if ($result) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $id = $row['id'];
+                    $name = $row['name'];
+                    $phone = $row['phone'];
 
 
-                ?>
-                <tr>
-                    <td> <?php echo $name ?></td>
-                    <td> <?php echo $phone ?></td>
-                    <td><a href = "edit.php?id=<?php echo $id; ?>">Update</a>
-                    <a href = "delete.php?id=<?php echo $id; ?>">Delete</a>
-                    </td>
-                </tr>
+            ?>
+                    <tr>
+                        <td> <?php echo $name ?></td>
+                        <td> <?php echo $phone ?></td>
+                        <td><a href="edit.php?id=<?php echo $id; ?>">Update</a>
+                            <a href="delete.php?id=<?php echo $id; ?>">Delete</a>
+                        </td>
+                    </tr>
 
-                <?php 
+            <?php
+                }
             }
-        }
-    ?>
-    </table>
+            ?>
+        </table>
+    </div>
 </body>
+
 </html>
